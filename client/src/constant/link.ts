3c6a4fd1bca9e0449132fd0f4e98c3998baf0cee
feyzibrahim <faizashrafkk@gmail.com>
2024-04-19 11:46:17 +0530

    deploy: changed backend domain id

--- client/src/constant/link.ts
+++ client/src/constant/link.ts
@@ -1,2 +1,2 @@
-// export const URL = 'https://weather-app-backend-rejx.onrender.com'
-export const URL = 'http://localhost:4000'
+export const URL = 'https://weather-app-backend-rejx.onrender.com'
+// export const URL = 'http://localhost:4000'
